dade2f0f806824df73c853ae6d6c85928d6a72de
gurpreet <gurpkaur37@gmail.com>
2025-06-09 22:51:18 +0530

    user model

--- models/User.js
+++ models/User.js
@@ -42,6 +42,14 @@ const userSchema=new mongoose.Schema({
     type:String,
     required:true,
   },
+  token:{
+    type:String,
+    required:true,
+  },
+  resetPasswordExpires:
+  {
+    type:Date,
+  },
   courseProgress:[
     {
       type:mongoose.Schema.Types.ObjectId,
